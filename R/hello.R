d3162fd42551d7e7b26a75ba23a9a8ae2f60cda4
athammad <ahmed.t.hammad@gmail.com>
2022-01-11 01:33:30 +0800

    first check

--- R/hello.R
+++ R/hello.R
@@ -1,11 +1,12 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
+
 #
 # Some useful keyboard shortcuts for package authoring:
 #
